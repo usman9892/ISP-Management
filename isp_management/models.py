84850c73292af9f138fbeee6032a432c7e558a48
usman9892 <uh9892@gmail.com>
2025-02-26 08:44:44 +0500

    Update models.py

--- isp_management/models.py
+++ isp_management/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Customer(models.Model):
     username = models.CharField(max_length=50, unique=True)
-    full_name = models.CharField(unique=True)(max_length=100)
-    phone_number = models.CharField(unique=True)(max_length=15)
+   full_name = models.CharField(max_length=100, unique=True)
+phone_number = models.CharField(max_length=15, unique=True)
     monthly_bill = models.DecimalField(max_digits=10, decimal_places=2)
     pending_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     is_active = models.BooleanField(default=True)
