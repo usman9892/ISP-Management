acf0df81776ea5dc8088a0302d9f57f386f826a1
usman9892 <uh9892@gmail.com>
2025-02-26 08:47:15 +0500

    Update views.py

--- isp_management/views.py
+++ isp_management/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Customer
 
 def customer_list(request):
-    customers = Customer.objects.only('id', 'name')
+   customers = Customer.objects.only('id', 'full_name', 'phone_number', 'pending_amount')
     return render(request, 'customer_list.html', {'customers': customers})
