681e1bbd2a57f8a0f222f21d63d677382c8dc977
usman9892 <uh9892@gmail.com>
2025-02-26 20:42:36 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isp_management.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+# Django settings module set karein (PyInstaller ke liye zaroori hai)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'isp_management.settings')
+
+# Django ko initialize karein
+import django
+django.setup()
